b531de69e8483c06716aab5998aa4cc0b1d0b39f
ADAN GIOVANNI RAMIREZ ERIVES <a1124250019@utch.edu.mx>
2024-08-02 11:30:14 -0600

    Cambios de Lupita añadidos a la rama principal

--- Quiz/Quiz.cs
+++ Quiz/Quiz.cs
@@ -14,11 +14,14 @@ namespace Quiz
         private string respuesta;
         private bool respuestaCorrecta = false;
 
-        // Variable que almacenará el número de preguntas aleatorias
-        private List<int> preguntasBasicas = new List<int>()
+        // Variable que almacenará el número de preguntas 
+        protected List<int> preguntasBasicas = new List<int>()
         {
-            1, 2, 3
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10
         };
+
+        public string Respuesta { get => respuesta; set => respuesta = value; }
+        public bool RespuestaCorrecta { get => respuestaCorrecta; set => respuestaCorrecta = value; }
 
         // Cada método de cada categoría generará UNA pregunta aleatoria de esa categoría dependiendo la dificultad
         public virtual void Basico(Concursante concursante)
