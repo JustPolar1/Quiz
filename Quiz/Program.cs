4d1ad03e217815a6100f9beabe3d247024f4698f
ADAN GIOVANNI RAMIREZ ERIVES <a1124250019@utch.edu.mx>
2024-08-04 21:44:35 -0600

    Creación del primer menú de selección de modo de juego, donde se le pregunta al usuario si quiere jugar en modo multijugador o en solitario :D

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,16 +11,58 @@ namespace Quiz
     {
         static void Main(string[] args)
         {
-            // Prueba, esto no va a estar en la versión final.
-            Concursante Avril = new Concursante("Avril");
+            // Definición de variables útiles para el programa
+            bool multijugador = false; // Nos hará saber si el juego es multijugador o singleplayer
+            bool seguirCiclo = true; // Varialbe útil para saber cuándo parar nuestros ciclos en la interfaz
+            int respuesta; // Variable para guardar las respuestas del usuario
 
-            Quiz quiz = new Quiz();
-            Ciencias quizCiencias = new Ciencias();
+            // Se le da la opción al usuario de jugar en modo multijugador o solitario
+            Console.WriteLine("¡Bienvenido al juego de Quizzes interactivo!");
+            do
+            {
+                try
+                {
+                    Console.WriteLine("¿En qué modo de juego te gustaría jugar?");
+                    Console.WriteLine("\t1. Solitario");
+                    Console.WriteLine("\t2. Multijugador");
+                    Console.Write("Escribe el número de la opción que quieres jugar: ");
+                    respuesta = int.Parse(Console.ReadLine());
+                    Console.Clear();
 
-            quizCiencias.Basico(Avril);
+                    switch (respuesta)
+                    {
+                        case 1: // Modo solitario
+                            multijugador = false;
+                            Console.WriteLine("¡Genial! elegiste el modo solitario");
+                            seguirCiclo = false;
+                            break;
+                        case 2: // Modo multijugador
+                            multijugador = true;
+                            Console.WriteLine("!Genial¡ elegiste el modo multijugador");
+                            seguirCiclo = false;
+                            break;
+                        default:
+                            Console.WriteLine($"Hermano, el número: {respuesta} no es una respuesta, por fa elige entre el 1 y el 2");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Favor de escribir solo los números 1 o 2");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Escribiste muchos números, casi haces explotar tu pc, no lo vuelvas a hacer");
+                }
+                finally
+                {
+                    Console.WriteLine();
+                    Console.Write("Presiona cualquier tecla para continuar: ");
+                    Console.ReadKey();
 
-            //Esto no lo pondremos en las preguntas, se pone al final
-            Console.WriteLine($"{Avril.Nombre} tiene: {Avril.Puntos} punto(s)");
+                    Console.Clear();
+                }
+            } while (seguirCiclo);
 
             Console.ReadKey();
         }
